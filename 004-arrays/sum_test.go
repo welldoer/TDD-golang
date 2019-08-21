fb5aa287fde4ea617415d8b8de5ab146e8ff4b86
welldoer <welldoer@263.net>
2019-08-21 14:36:35 +0800

    新增 SumAll 练习

--- 004-arrays/sum_test.go
+++ 004-arrays/sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"testing"
+	"reflect"
+)
 
 func TestSum(t *testing.T) {
 
@@ -26,5 +29,15 @@ func TestSum(t *testing.T) {
 		}
 	})
 
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
 
