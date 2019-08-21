afb8bc2d6dbcdd342423a29399f26809723be38b
welldoer <welldoer@263.net>
2019-08-21 19:21:21 +0800

    添加取钱行为

--- 006-pointers/wallet_test.go
+++ 006-pointers/wallet_test.go
@@ -6,14 +6,29 @@ import (
 
 func TestWallet(t *testing.T) {
 
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
-	want := Bitcoin(10)
+		got := wallet.Balance()
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
 
