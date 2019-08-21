59136ec7a221a5e523f304b19f6fd9d8d5b923e9
welldoer <welldoer@263.net>
2019-08-21 19:48:55 +0800

    测试代码优化

--- 006-pointers/wallet_test.go
+++ 006-pointers/wallet_test.go
@@ -27,16 +27,20 @@ func TestWallet(t *testing.T) {
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
+	assertError := func(t *testing.T, err error) {
+		t.Helper()
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	}
+
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
 		startingBalance := Bitcoin(20)
 		wallet := Wallet{startingBalance}
 		err := wallet.Withdraw(Bitcoin(100))
 
 		assertBalance(t, wallet, startingBalance)
-
-		if err == nil {
-			t.Error("wanted an error but didn't get one")
-		}
+		assertError(t, err)
 	})
 
 }
