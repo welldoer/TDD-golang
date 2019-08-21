313fa7bfec30c3d60a946ef6a4e410aca42ef5ca
welldoer <welldoer@263.net>
2019-08-21 16:40:32 +0800

    使用 struct 定义 Rectangle

--- 005-structs/shapes.go
+++ 005-structs/shapes.go
@@ -1,5 +1,15 @@
 package shapes
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+type Rectangle struct {
+	Width float64
+	Height float64
 }
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
+
--- 005-structs/shapes_test.go
+++ 005-structs/shapes_test.go
@@ -4,7 +4,8 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -12,3 +13,16 @@ func TestPerimeter(t *testing.T) {
 	}
 
 }
+
+func TestArea(t *testing.T) {
+
+	rectangle := Rectangle{12.0, 6.0}
+	got := Area(rectangle)
+	want := 72.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+
+}
+
