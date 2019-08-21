c2bffecc714b5938604d6ba2b3688fae076cea21
welldoer <welldoer@263.net>
2019-08-21 14:47:53 +0800

    编写 SumAllTails 练习

--- 004-arrays/sum.go
+++ 004-arrays/sum.go
@@ -17,3 +17,15 @@ func SumAll(numbersToSum ...[]int) []int {
 
 	return sums
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+
+	return sums
+}
+
